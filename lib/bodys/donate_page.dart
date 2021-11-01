ac5e7d102f94e2dced5833aa05760fa7d9668e23
teruru2017 <34904591+teruru2017@users.noreply.github.com>
2021-11-01 14:22:36 +0700

    Add files via upload
    up1/11/21

--- lib/bodys/donate_page.dart
+++ lib/bodys/donate_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:thanyarak/utility/my_constant.dart';
 import 'package:thanyarak/widgets/show_circular.dart';
 import 'package:thanyarak/widgets/show_title.dart';
@@ -11,6 +13,36 @@ class DonatePage extends StatefulWidget {
 }
 
 class _DonatePageState extends State<DonatePage> {
+  List<String> pathImageAritcles = [
+    'images/donate.png',
+    'images/donate.png',
+    'images/donate.png',
+    'images/donate.png'
+  ];
+  List<String> titleAritcles = [
+    'Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ111',
+    'Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ222',
+    'Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ333',
+    'Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ444',
+  ];
+  List<String> detailAritcles = [
+    'Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16',
+    'Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16',
+    'Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16',
+    'Lorem Ipsum คือ เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16'
+  ];
+  List<String> dataAritcles = [
+    '16-6-25564',
+    '17-6-25564',
+    '18-6-25564',
+    '18-6-25564'
+  ];
+  List<String> viewAritcles = [
+    'ผู้เข้าชม 123k',
+    'ผู้เข้าชม 123k',
+    'ผู้เข้าชม 123k',
+    'ผู้เข้าชม 123k'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +50,100 @@ class _DonatePageState extends State<DonatePage> {
         child: Column(
           children: [
             buildHead(),
+            listArticle(),
           ],
         ),
+      ),
+    );
+  }
+
+  SizedBox listArticle() {
+    return SizedBox(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        itemCount: pathImageAritcles.length,
+        itemBuilder: (context, index) => Card(
+            elevation: 3,
+            margin: EdgeInsets.all(8),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  height: 150,
+                  width: 280,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    image: DecorationImage(
+                      image: AssetImage(
+                        pathImageAritcles[index],
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: ShowTitle(
+                        title: cutWordH(titleAritcles[index]),
+                        textStyle: GoogleFonts.kanit(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff0088C6),
+                        )),
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: ShowTitle(
+                        title: cutWord(detailAritcles[index]),
+                        textStyle: GoogleFonts.kanit(
+                          fontSize: 12,
+                          color: Colors.black,
+                        )),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(16, 4, 4, 4),
+                          child: SvgPicture.asset('images/c1.svg'),
+                        ),
+                        ShowTitle(
+                          title: dataAritcles[index],
+                          textStyle: MyConstant().h4StyleGley(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(16, 4, 4, 4),
+                          child: SvgPicture.asset('images/v2.svg'),
+                        ),
+                        ShowTitle(
+                          title: viewAritcles[index],
+                          textStyle: MyConstant().h4StyleGley(),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            )),
       ),
     );
   }
@@ -63,4 +187,22 @@ class _DonatePageState extends State<DonatePage> {
       ),
     );
   }
+}
+
+String cutWord(String detailAritcl) {
+  String resutl = detailAritcl;
+  if (resutl.length >= 150) {
+    resutl = resutl.substring(0, 75);
+    resutl = '$resutl ....';
+  }
+  return resutl;
+}
+
+String cutWordH(String titleAritcles) {
+  String resutl = titleAritcles;
+  if (resutl.length >= 50) {
+    resutl = resutl.substring(0, 25);
+    resutl = '$resutl ....';
+  }
+  return resutl;
 }
