0b22af94b9017d2af214e7e7ceac3c75e4a7eaaa
teruru2017 <34904591+teruru2017@users.noreply.github.com>
2021-11-03 16:43:09 +0700

    up/main

--- lib/bodys/main_page.dart
+++ lib/bodys/main_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:thanyarak/bodys/about_page.dart';
 import 'package:thanyarak/bodys/shop_page.dart';
 // import 'package:thanyarak/bodys/about_page.dart';
 import 'package:thanyarak/bodys/menu_page.dart';
@@ -218,10 +219,10 @@ class _MainPageState extends State<MainPage> {
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(primary: Colors.white),
                     onPressed: () {
-                      // Navigator.push(
-                      //     context,
-                      //     CupertinoPageRoute(
-                      //         builder: (context) => AboutPage()));
+                      Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => AboutPage()));
                     },
                     child: ShowTitle(
                       title: 'ดูทั้งหมด',
