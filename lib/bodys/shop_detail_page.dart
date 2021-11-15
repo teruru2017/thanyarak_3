ad24391becbabe90c72e48268da1583f5628eb95
teruru2017 <34904591+teruru2017@users.noreply.github.com>
2021-11-15 17:48:41 +0700

    edit/ShopDetailPage/15/11/64

--- lib/bodys/shop_detail_page.dart
+++ lib/bodys/shop_detail_page.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:thanyarak/bodys/shop_page.dart';
 import 'package:thanyarak/utility/my_constant.dart';
 import 'package:thanyarak/widgets/show_title.dart';
+
+import 'main_login_page.dart';
 
 class ShopDetailPage extends StatefulWidget {
   ShopDetailPage({Key? key}) : super(key: key);
@@ -42,7 +46,137 @@ class _ShopDetailPageState extends State<ShopDetailPage> {
             buildHead(),
             buildMigH(),
             listImages(),
-            listArticle(),
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Text(
+                        'หลายๆ ท่านสงสัย ตรวจเต้านมด้วยแมมโมแกรมแล้ว ทำไมยังต้องตรวจอัลตร้าซาวนด์อีกล่ะ ',
+                        style: GoogleFonts.kanit(
+                          textStyle: Theme.of(context).textTheme.headline4,
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff0088c6),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(2, 4, 4, 4),
+                            child: SvgPicture.asset('images/c1.svg'),
+                          ),
+                          Text(
+                            'e.dateTime',
+                            style: GoogleFonts.kanit(
+                                color: Colors.grey.withOpacity(0.8)),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(6, 4, 4, 4),
+                            child: SvgPicture.asset('images/v2.svg'),
+                          ),
+                          Text(
+                            'e.dateTime',
+                            style: GoogleFonts.kanit(
+                                color: Colors.grey.withOpacity(0.8)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              width: 370,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 6, 6, 6),
+                child: ShowTitle(
+                  title: cutWord(
+                    'หลายๆ ท่านสงสัย ตรวจเต้านมด้วยแมมโมแกรมแล้ว ทำไมยังต้องตรวจอัลตร้าซาวนด์อีกล่ะ มันให้ผลการตรวจวินิจฉัยแตกต่างกันอย่างไร เรามีสาระความรู้มาฝากค่ะหลายๆ ท่านสงสัย ตรวจเต้านมด้วยแมมโมแกรมแล้ว ทำไมยังต้องตรวจอัลตร้าซาวนด์อีกล่ะ มันให้ผลการตรวจวินิจฉัยแตกต่างกันอย่างไร เรามีสาระความรู้มาฝากค่ะ',
+                  ),
+                  textStyle: MyConstant().h2StyleBack(),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 16,
+                ),
+                Text(
+                  'ราคา',
+                  style: GoogleFonts.kanit(
+                      fontSize: 18, color: Colors.grey.withOpacity(0.8)),
+                ),
+                SizedBox(
+                  width: 250,
+                ),
+                Text(
+                  '210',
+                  style: GoogleFonts.kanit(
+                    textStyle: Theme.of(context).textTheme.headline4,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xff000000),
+                  ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  'บาท',
+                  style: GoogleFonts.kanit(
+                    textStyle: Theme.of(context).textTheme.headline4,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xff000000),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    CupertinoPageRoute(
+                        builder: (context) =>
+                            MainLoginPage())); //SignInPage()));
+              },
+              child: Container(
+                width: 320,
+                padding: EdgeInsets.symmetric(vertical: 15),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Color(0xffE6EFFE),
+                  gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [Color(0xff0088C6), Color(0xff43CEF8)]),
+                ),
+
+                //Image.asset("images/i_shoe_bt.png"),
+
+                child: Text(
+                  "ช้อปเลย",
+                  style: GoogleFonts.kanit(
+                    textStyle: Theme.of(context).textTheme.headline4,
+                    fontSize: 16,
+                    color: Color(0xffFFFFFF),
+                    // fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -76,18 +210,30 @@ class _ShopDetailPageState extends State<ShopDetailPage> {
         trailing: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.notifications,
-                color: Colors.white,
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    CupertinoPageRoute(builder: (context) => ShopPage()));
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        scale: 1.3,
+                        image: AssetImage("images/h_shop_d_1.png"),
+                        alignment: Alignment(-0.7, 0.6))),
               ),
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.menu,
-                color: Colors.white,
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    CupertinoPageRoute(builder: (context) => ShopPage()));
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        scale: 1.3,
+                        image: AssetImage("images/h_shop_d2.png"),
+                        alignment: Alignment(-0.7, 0.6))),
               ),
             ),
           ],
@@ -136,85 +282,71 @@ class _ShopDetailPageState extends State<ShopDetailPage> {
         physics: const ClampingScrollPhysics(),
         itemCount: pathImageAritcles.length,
         itemBuilder: (context, index) => Card(
-            elevation: 3,
-            margin: EdgeInsets.all(8),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 170,
+              width: 366,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+                image: DecorationImage(
+                  image: AssetImage(
+                    pathImageAritcles[index],
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              width: 326,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: ShowTitle(
+                  title: cutWordH(titleAritcles[index]),
+                  textStyle: MyConstant().h2StyleBlue(),
+                ),
+              ),
+            ),
+            Row(
               children: [
-                Container(
-                  margin: EdgeInsets.all(10),
-                  height: 150,
-                  width: 280,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20)),
-                    image: DecorationImage(
-                      image: AssetImage(
-                        pathImageAritcles[index],
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 4, 4, 4),
+                  child: SvgPicture.asset('images/c1.svg'),
                 ),
-                Container(
-                  width: 300,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
-                    child: ShowTitle(
-                        title: cutWordH(titleAritcles[index]),
-                        textStyle: GoogleFonts.kanit(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                          color: Color(0xff0088C6),
-                        )),
-                  ),
+                ShowTitle(
+                  title: dataAritcles[index],
+                  textStyle: MyConstant().h4StyleGley(),
                 ),
-                Container(
-                  width: 300,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
-                    child: ShowTitle(
-                        title: cutWord(detailAritcles[index]),
-                        textStyle: GoogleFonts.kanit(
-                          fontSize: 12,
-                          color: Colors.black,
-                        )),
-                  ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 4, 4, 4),
+                  child: SvgPicture.asset('images/v2.svg'),
                 ),
-                SizedBox(
-                  height: 5,
+                ShowTitle(
+                  title: viewAritcles[index],
+                  textStyle: MyConstant().h4StyleGley(),
                 ),
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(16, 4, 4, 4),
-                          child: SvgPicture.asset('images/c1.svg'),
-                        ),
-                        ShowTitle(
-                          title: dataAritcles[index],
-                          textStyle: MyConstant().h4StyleGley(),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(16, 4, 4, 4),
-                          child: SvgPicture.asset('images/v2.svg'),
-                        ),
-                        ShowTitle(
-                          title: viewAritcles[index],
-                          textStyle: MyConstant().h4StyleGley(),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
               ],
-            )),
+            ),
+            Container(
+              width: 326,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: ShowTitle(
+                  title: cutWord(detailAritcles[index]),
+                  textStyle: MyConstant().h4StyleBlack(),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+          ],
+        )),
       ),
     );
   }
